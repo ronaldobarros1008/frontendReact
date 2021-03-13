3a5d271afa4bf6260335c7cd7138f1ad2db96a33
ronaldobarros1008 <ronaldobarros1008@gmail.com>
2021-03-13 03:59:08 -0300

    Commit Teste

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './main/App';
 import registerServiceWorker from './registerServiceWorker';
 
+//Teste
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
